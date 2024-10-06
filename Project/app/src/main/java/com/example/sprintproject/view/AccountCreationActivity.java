9c5bc3afe8cd4aea96f8148fbfcbad9a772ee339
yasharacha <yasharacha@gmail.com>
2024-10-06 17:27:40 -0400

    fixing MVVM structure for login

--- Project/app/src/main/java/com/example/sprintproject/view/AccountCreationActivity.java
+++ Project/app/src/main/java/com/example/sprintproject/view/AccountCreationActivity.java
@@ -13,17 +13,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.example.sprintproject.R;
+import com.example.sprintproject.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.example.sprintproject.viewmodel.userViewModel;
 
 public class AccountCreationActivity extends AppCompatActivity {
 
-    private FirebaseAuth mAuth;
-    private String username;
-    private String password;
+    private userViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,43 +33,22 @@ public class AccountCreationActivity extends AppCompatActivity {
         Button registerButton = findViewById(R.id.register_button);
         Button loginButton = findViewById(R.id.login_button);
 
-        mAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
         EditText usernameEditText = findViewById(R.id.username_edittext);
         EditText passwordEditText = findViewById(R.id.password_edittext);
 
-        // Register Button click
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                username = usernameEditText.getText().toString().trim();
-                password = passwordEditText.getText().toString().trim();
-                if (username.isEmpty() || username.contains(" ")) {
-                    Toast.makeText(AccountCreationActivity.this, "Please enter a valid username", Toast.LENGTH_SHORT).show();
-                } else if (password.isEmpty() || password.contains(" ")) {
-                    Toast.makeText(AccountCreationActivity.this, "Please enter a valid password", Toast.LENGTH_SHORT).show();
-                } else {
-                    username = username + "@gmail.com";
-                    mAuth.createUserWithEmailAndPassword(username, password)
-                            .addOnCompleteListener(AccountCreationActivity.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        // Sign in success, update UI with the signed-in user's information
-                                        Log.d("success", "createUserWithEmail:success");
-                                        FirebaseUser user = mAuth.getCurrentUser();
-                                        Toast.makeText(AccountCreationActivity.this, "Account successfully created", Toast.LENGTH_SHORT).show();
-                                    } else {
-                                        // If sign in fails, display a message to the user.
-                                        Log.w("failure", "createUserWithEmail:failure", task.getException());
-                                        Toast.makeText(AccountCreationActivity.this, "Account already exists", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                            });
-                }
+                User user = new User();
+                user.setUsername(usernameEditText.getText().toString().trim());
+                user.setPassword(passwordEditText.getText().toString().trim());
+                userViewModel.register(user);
             }
         });
 
+        /*
         // Navigate to a different activity on Login Button click
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,5 +57,8 @@ public class AccountCreationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+         */
     }
 }
