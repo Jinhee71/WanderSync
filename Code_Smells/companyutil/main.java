99fed9496c0797425cf10020a5fddcc7536262cb
yasharacha <yasharacha@gmail.com>
2024-11-03 19:56:43 -0500

    Fixed minor bug in main.java

--- Code_Smells/companyutil/main.java
+++ Code_Smells/companyutil/main.java
@@ -2,7 +2,7 @@ package companyutil;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class main {
 	public static void main(String[] args) {
         Item item1 = new Item("Book", 20, 1, DiscountType.AMOUNT, 5);
         Item item2 = new TaxableItem("Laptop", 1000, 1, DiscountType.PERCENTAGE, 0.1);
